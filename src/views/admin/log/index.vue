d5ad81671a22a1c3b53cd99b7c6dc915317331c2
zxgangandy <286505491@qq.com>
2019-02-24 23:43:45 +0800

    日志页面；

---
+++ src/views/admin/log/index.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>
+    <div class="search-container">
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import { delObj, fetchList } from '@/api/log'
+
+  export default {
+    data() {
+
+    }
+  }
+
+</script>
